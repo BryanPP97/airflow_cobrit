1cca43eeae1df4e65b175799050797c726ca5ca7
CopervaAI <130609710+CopervaAI@users.noreply.github.com>
2023-09-25 14:53:06 -0600

    sms dags

--- dags/sms_marcatel.py
+++ dags/sms_marcatel.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 #import scrapers
-from utils.gepard import gepard_automation, process_sms
+from utils.marcatel import marcatel_automation, process_sms
 from utils.utils_general import email, clean_folder
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -16,49 +16,37 @@ default_args={
     "email": ["jmontan@coperva.com"],
     "email_on_failure": True,
     "email_on_retry": False,
-    "retries": 2,
+    "retries": 1,
     "retry_delay": timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function, # or list of functions
-    # 'on_success_callback': some_other_function, # or list of functions
-    # 'on_retry_callback': another_function, # or list of functions
-    # 'sla_miss_callback': yet_another_function, # or list of functions
-    # 'trigger_rule': 'all_success'
 }
 
 ## Create DAG
 
 with DAG(
-    "SMS_GEPARD",
+    "SMS_MARCATEL",
     default_args=default_args,
-    description="Sends email for positive sms using gepard data",
-    start_date= datetime(year=2023, month=9, day=21),
+    description="Sends email for positive sms using marcatel data",
+    start_date= datetime(year=2023, month=9, day=25),
     schedule_interval="0 8 * * 1-5",
     tags = ['sms']
 ) as dag:
     scraper = PythonOperator(
-        task_id="scraper_gepard", 
-        python_callable=gepard_automation
+        task_id="scraper_marcatel", 
+        python_callable=marcatel_automation
         )
     processor = PythonOperator(
         task_id='processor',
         python_callable=process_sms
     )
     mailing = PythonOperator(
-        task_id="mailing", 
+        task_id='mailing',
         python_callable=email,
-        op_kwargs={'portal': 'Gepard'}
+        op_kwargs={'portal': 'Marcatel'}
     )
     cleaner = PythonOperator(
         task_id="cleaner",
         python_callable=clean_folder,
-        op_kwargs={'folder': '/opt/airflow/outputs/Gepard/'}
+        op_kwargs={'folder': '/opt/airflow/outputs/Marcatel/'}
     )
 
     scraper >> processor >> mailing >> cleaner
