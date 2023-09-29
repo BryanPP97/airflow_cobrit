e4617a3489e58629cb0c1727a90356509f5cdc29
CopervaAI <130609710+CopervaAI@users.noreply.github.com>
2023-09-29 11:24:59 -0600

    Update sfh_scraper.py

--- dags/sfh_scraper.py
+++ dags/sfh_scraper.py
@@ -25,7 +25,7 @@ with DAG(
     "SHF_Scraper",
     default_args=default_args,
     description="Download shf data",
-    start_date= datetime(year=2023, month=9, day=27, tzinfo=local_tz),
+    start_date= datetime(year=2023, month=9, day=29, tzinfo=local_tz),
     schedule_interval="@monthly",
     tags = ['sms']
 ) as dag:
