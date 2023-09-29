d3397622e634ece2675a9a6ba9842ca253434004
CopervaAI <130609710+CopervaAI@users.noreply.github.com>
2023-09-28 21:32:59 -0600

    Update SHF.py

--- dags/utils/SHF.py
+++ dags/utils/SHF.py
@@ -17,6 +17,7 @@ def ift_scraper():
     
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
     #chrome_options.add_argument('--disable-dev-shm-usage')   
     chrome_options.add_experimental_option("prefs", {
     "download.prompt_for_download": False,  # Desactiva la ventana emergente de descarga
