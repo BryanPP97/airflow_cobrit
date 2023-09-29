7a73c898c2f558462c779450eb7ccb38ab8d2e27
CopervaAI <130609710+CopervaAI@users.noreply.github.com>
2023-09-29 12:06:49 -0600

    Create ift_screaper.py

---
+++ dags/ift_screaper.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+import pendulum
+
+#import scrapers
+from utils.IFT import ift_automation
+from utils.utils_general import clean_folder
+from airflow import DAG
+
+# Python operator to run scripts
+from airflow.operators.python_operator import PythonOperator
+
+# Configuration
+default_args={
+    'owner': 'cobrit',
+    "depends_on_past": False,
+    "email": ["jmontan@coperva.com"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+local_tz = pendulum.timezone("America/Mexico_City")
+with DAG(
+    "IFT_Scraper",
+    default_args=default_args,
+    description="Download ift data",
+    start_date= datetime(year=2023, month=9, day=29, tzinfo=local_tz),
+    schedule_interval="@monthly",
+    tags = ['scraper', 'data']
+) as dag:
+    scraper = PythonOperator(
+        task_id="scraper", 
+        python_callable=ift_automation
+        )
+    cleaner = PythonOperator(
+        task_id="cleaner",
+        python_callable=clean_folder,
+        op_kwargs={'folder': '/opt/airflow/outputs/ift/'}
+    )
+
+    cleaner >> scraper
